c3f890b9b5f5c7bd0b92d579534e4b32783fe28f
Zap <67962871+ziap@users.noreply.github.com>
2025-07-09 19:55:17 +0700

    Create src subdirectory

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #/bin/sh -e
 
-for file in *.typ; do
+for file in src/*.typ; do
   base=$(basename -s .typ $file)
   out_pdf="out/$base.pdf"
 
   echo "Compiling $file -> $out_pdf"
-  typst compile --font-path template/fonts/ "$file" "$out_pdf"
+  typst compile --font-path src/template/fonts/ "$file" "$out_pdf"
 done
